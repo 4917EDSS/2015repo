54315fc3796236f81fb693a2ca601011cb3e956d
ShawnBowman <shawn.bowman77@gmail.com>
2015-02-02 18:23:14 -0500

    Quick Edits
    Changed Floats to decimals from ints.

--- 2015CompetitionBot/src/Commands/IntakeUntilLimitCmd.cpp
+++ 2015CompetitionBot/src/Commands/IntakeUntilLimitCmd.cpp
@@ -1,8 +1,9 @@
 #include "IntakeUntilLimitCmd.h"
+#include "../Subsystems/IntakeSub.h"
 
 IntakeUntilLimitCmd::IntakeUntilLimitCmd()
 {
-	Requires (rIntakeSub);
+	Requires(rIntakeSub);
 	// Use Requires() here to declare subsystem dependencies
 	// eg. Requires(chassis);
 }
@@ -10,7 +11,7 @@ IntakeUntilLimitCmd::IntakeUntilLimitCmd()
 // Called just before this Command runs the first time
 void IntakeUntilLimitCmd::Initialize()
 {
-	rIntakeSub->SetBeltsIn(1);
+	rIntakeSub->SetBeltsIn(1.0);
 }
 
 // Called repeatedly when this Command is scheduled to run
@@ -28,7 +29,7 @@ bool IntakeUntilLimitCmd::IsFinished()
 // Called once after isFinished returns true
 void IntakeUntilLimitCmd::End()
 {
-	rIntakeSub->SetBeltsIn(0);
+	rIntakeSub->SetBeltsIn(0.0);
 }
 
 // Called when another command which requires one or more of the same
