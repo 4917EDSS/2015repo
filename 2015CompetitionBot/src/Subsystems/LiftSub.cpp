58a38b114fae4c594174fa7c3ca3bf2b2f009f97
Gord Maier <gord@maierweb.ca>
2015-02-16 11:04:43 -0500

    Fixed compile issue

--- 2015CompetitionBot/src/Subsystems/LiftSub.cpp
+++ 2015CompetitionBot/src/Subsystems/LiftSub.cpp
@@ -119,7 +119,7 @@ void LiftSub::LiftMotorUp(float speed)
 	else if(GetArmHeight() >= INTERFERENCE_LOCKOUT_EV && GetJaws() == JAWS_CLOSED && speed > 0){
 		liftMotor->Set(0);
 	}
-	else if ((speed < 0) && (GetArmHeight <= SCORE_HEIGHT_EV)){
+	else if ((speed < 0) && (GetArmHeight() <= SCORE_HEIGHT_EV)){
 		liftMotor->Set(SLOW_MAST_SPEED);
 	}
 	else
