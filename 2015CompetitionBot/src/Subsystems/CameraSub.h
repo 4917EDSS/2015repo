256610ad5e63fe4ea4e5da0e191dde37c1a79ba8
4917 programmer <notanemail@asdfasdfasdf.qqq>
2015-02-17 20:11:11 -0500

    More camera changes

--- 2015CompetitionBot/src/Subsystems/CameraSub.h
+++ 2015CompetitionBot/src/Subsystems/CameraSub.h
@@ -6,12 +6,20 @@
 
 class CameraSub: public Subsystem
 {
+
 private:
+	IMAQdxSession session;
+	Image *frame;
+	IMAQdxError imaqError;
+
 	// It's desirable that everything possible under private except
 	// for methods that implement subsystem capabilities
 public:
 	CameraSub();
 	void InitDefaultCommand();
+	void RunFeed();
+	void StartFeed();
+	void EndFeed();
 };
 
 #endif
