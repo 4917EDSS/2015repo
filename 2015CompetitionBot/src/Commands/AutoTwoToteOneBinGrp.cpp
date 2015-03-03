5553870f59fd10a2af1f5e60057bdfff69194591
ShawnBowman <shawn.bowman77@gmail.com>
2015-03-02 19:44:14 -0500

    Modified AutoTwoToteOneBin to work
    Modified AutoTwoToteOneBin so that it will actually work.

--- 2015CompetitionBot/src/Commands/AutoTwoToteOneBinGrp.cpp
+++ 2015CompetitionBot/src/Commands/AutoTwoToteOneBinGrp.cpp
@@ -1,43 +1,43 @@
+#include <Commands/Turn90CCWiseDropGrp.h>
 #include "AutoTwoToteOneBinGrp.h"
 #include "RobotParameters.h"
+#include "PickUpBoxGrp.h"
 #include "DriveStraightCmd.h"
+#include "IntakeUntilLimitCmd.h"
 #include "DriveTurnCmd.h"
+#include "AutoDropStackGrp.h"
 #include "SetLiftHeightCmd.h"
 #include "SetArmsCmd.h"
-#include "SetJawsCmd.h"
 #include "SetLocksCmd.h"
-#include "GrabStackGrp.h"
+#include "SetJawsCmd.h"
+#include "PickUpFirstBoxGrp.h"
 #include "ResetLiftEncoderCmd.h"
-#include "IntakeUntilLimitCmd.h"
-#include "AutoDriveForwardsGrp.h"
-#include "AutoDropStackGrp.h"
+#include "GrabStackGrp.h"
 
 
 AutoTwoToteOneBinGrp::AutoTwoToteOneBinGrp()
 {
+
 	AddParallel(new SetLocksCmd(LOCKS_OPEN));
-	AddSequential(new SetJawsCmd(JAWS_OPEN));
-	AddSequential(new SetJawsCmd(ARMS_OPEN));
-	AddSequential(new SetLiftHeightCmd(GREEN_BIN_ABOVE_TOTE_EV));
-	AddSequential(new DriveStraightCmd(DRIVE_ONE_GREEN_BIN, MAX_SPEED_EV/2));
-	AddSequential(new ResetLiftEncoderCmd());
-	AddSequential(new SetArmsCmd(ARMS_CLOSED));
-	AddSequential(new WaitCommand(ARMS_CLOSE_DELAY));
-	AddParallel(new DriveStraightCmd(DISTANCE_BETWEEN_TOTES, (float)MAX_SPEED_EV/2));
-	AddSequential(new SetLiftHeightCmd(TRANSFER_EV));
-	AddSequential(new SetLocksCmd(LOCKS_CLOSED));
-	AddSequential(new WaitCommand(LOCKS_CLOSE_DELAY));
-	AddSequential(new SetArmsCmd(ARMS_OPEN));
-	AddSequential(new WaitCommand(ARMS_OPEN_DELAY));
-	AddSequential(new ResetLiftEncoderCmd());
-	AddSequential(new SetArmsCmd(ARMS_CLOSED));
-	AddSequential(new SetJawsCmd(JAWS_CLOSED));
-	AddSequential(new IntakeUntilLimitCmd());
-	AddSequential(new SetLiftHeightCmd(SCORE_HEIGHT_EV));
-	AddSequential(new DriveTurnCmd(90, true, MAX_SPEED_EV/2));
-	AddSequential(new DriveStraightCmd(DRIVE_CENTER_TO_CENTER, (float)MAX_SPEED_EV/2));
-	AddSequential(new AutoDropStackGrp());
-	AddSequential(new DriveStraightCmd(BACK_OFF_DISTANCE, MAX_SPEED_EV/2));
+		AddSequential(new SetJawsCmd(JAWS_OPEN));
+		AddSequential(new SetArmsCmd(ARMS_OPEN));
+		AddSequential(new SetLiftHeightCmd(GREEN_BIN_ABOVE_TOTE_EV));
+		AddSequential(new DriveStraightCmd(DRIVE_ONE_GREEN_BIN, MAX_SPEED_EV));
+		AddSequential(new ResetLiftEncoderCmd());
+		AddSequential(new SetArmsCmd(ARMS_CLOSED));
+		AddSequential(new IntakeUntilLimitCmd());
+		AddParallel(new DriveStraightCmd(DISTANCE_BETWEEN_TOTES, MAX_SPEED_EV/2));
+		AddSequential(new SetLiftHeightCmd(TRANSFER_EV));
+		AddSequential(new SetJawsCmd(JAWS_CLOSED));
+		AddSequential(new WaitCommand(JAWS_CLOSE_DELAY));
+		AddSequential(new SetLocksCmd(LOCKS_CLOSED));
+		AddSequential(new SetArmsCmd(ARMS_OPEN));
+		AddSequential(new WaitCommand(ARMS_OPEN_DELAY));
+		AddSequential(new ResetLiftEncoderCmd());
+		AddSequential(new SetArmsCmd(ARMS_CLOSED));
+		AddSequential(new IntakeUntilLimitCmd());
+		AddParallel(new GrabStackGrp());
+		AddSequential(new Turn90CCWiseDropGrp());
 
 
 	// Add Commands here:
