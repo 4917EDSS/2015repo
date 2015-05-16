de071e10299f1a64e6aab0f84774d2c0ba57bc39
Sam Maier <sam@sammaier.ca>
2015-05-15 21:52:53 -0400

    Removed CameraSub

--- 2015CompetitionBot/src/CommandBase.cpp
+++ 2015CompetitionBot/src/CommandBase.cpp
@@ -6,7 +6,6 @@ OI* CommandBase::oi = NULL;
 DrivetrainSub* CommandBase::rDrivetrainSub = NULL;
 IntakeSub* CommandBase::rIntakeSub = NULL;
 LiftSub* CommandBase::rLiftSub = NULL;
-//CameraSub* CommandBase::rCameraSub = NULL;
 
 CommandBase::CommandBase(char const *name) :
 		Command(name)
@@ -27,7 +26,6 @@ void CommandBase::init()
 	rDrivetrainSub = new DrivetrainSub(RightDrivePWM, LeftDrivePWM, LeftDriveEncoder1DIO, LeftDriveEncoder2DIO, RightDriveEncoder1DIO, RightDriveEncoder2DIO);
 	rIntakeSub = new IntakeSub(ArmRightMotorPWM, ArmLeftMotorPWM, ArmsSolenoid1PNC, ArmsSolenoid2PNC, IntakeLimitSwitchDIO);
 	rLiftSub = new LiftSub(LiftMotorPWM, LockSolenoid1PNC, LockSolenoid2PNC, JawsSolenoid1PNC, JawsSolenoid2PNC, LiftEncoder1DIO, LiftEncoder2DIO, TopLimitSwitchDIO, BottomLimitSwitchDIO);
-	//rCameraSub = new CameraSub();
 
 
 
